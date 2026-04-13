4569c4e73fc6cb0d355c5d2c604413fc008976af
Arjun Verma <arjun0942.becse24@chitkara.edu.in>
2026-04-13 11:48:00 +0530

    Solved 3Sum

---
+++ Array/3sum.java
@@ -0,0 +1,30 @@
+/*
+Problem: 3Sum
+Platform: LeetCode
+Link: https://leetcode.com/problems/3sum/
+Topic: Arrays,Two-Pointer
+*/
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    for(int i = 0; i<nums.length; i++){
+        if( i > 0 && nums[i] == nums[i-1]) continue; // Condition for checking duplicates
+        int left = i+1;
+        int right = nums.length - 1;
+        while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+            if(sum == 0){
+                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                left++;
+                right--;
+                while(left < right && nums[left] == nums[left-1])left++; //Checking duplicates
+                while(left < right && nums[right] == nums[right+1])right--; //Checking duplicates
+            }
+            else if (sum > 0) right--;
+            else left++;
+        }
+    }
+    return ans;
+    }
+}
